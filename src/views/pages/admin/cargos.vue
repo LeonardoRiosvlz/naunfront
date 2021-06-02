3701635266088745f1f2385e91bdcca8f8750a6f
leonardo <leonardo27188@gmail.com>
2021-06-01 23:44:12 -0400

    cargos

--- src/views/pages/admin/cargos.vue
+++ src/views/pages/admin/cargos.vue
@@ -2,7 +2,7 @@
   <Layout>
     <PageHeader :title="title" :items="items" />
     <div class="clearfix mb-3">
-      <b-button class="float-right btn-info" left @click="$bvModal.show('modal');editMode=false;">Crear Cargo</b-button>
+      <b-button class="float-right btn-info" left @click="$bvModal.show('modal');editMode=false;ver=false;resete();">Crear Cargo</b-button>
     </div>
     <div class="row">
       <div class="col-12">
@@ -48,8 +48,14 @@
                 :filter-included-fields="filterOn"
                 @filtered="onFiltered"
               >
-                <template v-slot:cell(actions)="data">
-
+              <template v-slot:cell(usuario)="data">
+                {{data.item.user.nombre}}
+              </template>
+              <template v-slot:cell(status)="data">
+                  <b-badge v-if="data.item.status === 'Activo'" variant="success">ACTIVO </b-badge>
+                  <b-badge v-else variant="danger">INACTIVO</b-badge>
+              </template>
+              <template v-slot:cell(actions)="data">
                 <b-dropdown size="sm" class="">
                   <template v-slot:button-content>
                     Action
@@ -84,18 +90,20 @@
                 <div class="form-group">
                   <label>Nombre</label>
                   <ValidationProvider name="cargo" rules="required|alpha_spaces" v-slot="{ errors }">
-                        <input v-model="form.cargo"  type="text" class="form-control" placeholder=" " :disabled="ver">
+                        <input v-model="form.nombre"  type="text" class="form-control" placeholder=" " :disabled="ver">
                         <span style="color:red">{{ errors[0] }}</span>
                   </ValidationProvider>
                 </div>
               </b-col>
+              </b-row>
+              <b-row>
               <b-col>
                 <div class="form-group">
                 <label>Estado</label>
                 <ValidationProvider name="estado" rules="required" v-slot="{ errors }">
                     <select v-model="form.status"  name="tipo" class="form-control form-control-lg" >
-                        <option value="Administrador">Activo</option>
-                        <option value="Coordinador">No activo</option>
+                        <option value="Activo">Activo</option>
+                        <option value="Inactivo">Ianactivo</option>
                     </select>
                     <span style="color:red">{{ errors[0] }}</span>
                 </ValidationProvider>
@@ -112,17 +120,26 @@
                   </ValidationProvider>
                 </div>
               </b-col>
-              </b-row>         
+              </b-row>
+              <b-row>
+              <b-col>
+                <ValidationProvider name="area dependiente" rules="required" v-slot="{ errors }">
+                  <label>Usuario</label>
+                    <v-select v-model="form.user_id" :options="usuarios" :reduce="usuarios => usuarios.user.id"  :getOptionLabel="option => option.nombre+' '+option.user.status" ></v-select>
+                    <span style="color:red">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </b-col>
+            </b-row>         
         </ValidationObserver>
-        <button class="btn btn-block float-right btn-success" @click="switchLoc" v-if="!ver && !editMode">Guardar</button>
+        <button class="btn btn-block float-right btn-success my-2" @click="switchLoc" v-if="!ver && !editMode">Guardar</button>
         <button class="btn btn-block float-right btn-success" @click="switchLoc" v-if="!ver && editMode">Editar</button>
      </b-modal>
-
   </Layout>
 </template>
 
 <script>
-
+import "vue-select/dist/vue-select.css";
+import vSelect from "vue-select";
 import vue2Dropzone from "vue2-dropzone";
 import {mapState,mapMutations, mapActions} from 'vuex'
 import { ValidationProvider, ValidationObserver } from "vee-validate";
@@ -139,7 +156,8 @@ export default {
     Layout,
     PageHeader,
     ValidationProvider,
-    ValidationObserver
+    ValidationObserver,
+    vSelect
   },
   data() {
     return {
@@ -174,26 +192,26 @@ export default {
       filterOn: [],
       sortBy: "age",
       sortDesc: false,
-      fields: ["cargo","descripcion","actions"],
+      fields: ["nombre","descripcion","usuario","status","actions"],
       cargos: [], 
+      perfil: [], 
+      usuarios: [],
       editMode:false,
       form:{
-          "id": 6,
+          "id": "",
           "nombre": "",
           "descripcion": "",
-          "status": null,
+          "status": "",
           "created_at": "",
           "updated_at": "",
-          "cliente_id": 1,
+          "cliente_id": "",
+          "user_id": "",
       }
     }
   },
   computed:{
         ...mapState(['counter'])
    },
-  created(){
-    this.listarUsers();
-  },
   methods: {
 
     onFiltered(filteredItems) {
@@ -304,22 +322,39 @@ export default {
         for (var key in formulario) {
              this.form[key]="";
        }
+       this.form.cliente_id=this.cliente.id;
       },
       setear(id) {
         for (let index = 0; index < this.cargos.length; index++) {
           if (this.cargos[index].id===id) {
             this.form.id=this.cargos[index].id;
-            this.form.cargo=this.cargos[index].cargo;
+            this.form.nombre=this.cargos[index].nombre;
+            this.form.user_id=this.cargos[index].user_id;
+            this.form.status=this.cargos[index].status;
             this.form.descripcion=this.cargos[index].descripcion;
             this.$root.$emit("bv::show::modal", "modal", "#btnShow");
             return;
           }
         }
       },
-    async  listarCargos(){
-       await this.axios.get('api/cargos')
+   async listarCargos(){
+     let data = new FormData();
+     data.append('cliente_id',this.cliente.id);
+       await this.axios.post('api/cargos/listar',data)
         .then((response) => {
-          this.cargos = response.data.rows;
+          this.cargos = response.data;
+          this.listarperfil();
+        })
+        .catch((e)=>{
+          console.log('error' + e);
+        })
+      },
+   async listarperfil(){
+     let data = new FormData();
+     data.append('cliente_id',this.cliente.id);
+       await this.axios.post('api/perfil/lista',data)
+        .then((response) => {
+          this.usuarios = response.data;
         })
         .catch((e)=>{
           console.log('error' + e);
@@ -358,21 +393,28 @@ export default {
           this.$router.push({ name: 'Home' });
           }
         }
-  },
+  }, 
   watch: {
       cliente: function () {
        this.listarCargos();
+        this.title=this.cliente.nombre_prestador;
+        this.form.cliente_id=this.cliente.nombre_prestador;
       },
     },
+    mounted(){
+        
+    },
+
     created(){
         this.session();
         this.listarCargos();
-
+        
       },
      mounted() {
 
     },
     computed: {
+       ...mapState(['usuarioDB','cliente']),
     rows() {
       return this.cargos.length;
     },
