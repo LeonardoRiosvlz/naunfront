8737d858e38cbdc9b117192159615b44d0e1bcd2
Luis Angel <luis.angel151199@gmail.com>
2021-06-17 11:30:18 -0400

    plantillas created

---
+++ src/views/pages/admin/plantillas.vue
@@ -0,0 +1,276 @@
+<template>
+  <Layout>
+    <PageHeader :title="title" :items="items" />
+    <div class="clearfix mb-3">
+      <b-button class="float-right btn-info" left @click="$bvModal.show('modal');editMode=false;resete();">Crear Plantilla</b-button>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+              <Editor/>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+  </Layout>
+</template>
+
+<script>
+import vue2Dropzone from "vue2-dropzone";
+import {mapState,mapMutations, mapActions} from 'vuex'
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import Layout from "../../layouts/main";
+import PageHeader from "@/components/page-header";
+import Editor from "@/components/editor";
+
+
+/**
+ * Dashboard component
+ */
+export default {
+  components: {
+    vueDropzone: vue2Dropzone,
+    Layout,
+    PageHeader,
+    ValidationProvider,
+    ValidationObserver,
+    Editor
+  },
+  data() {
+    return {
+      title: "Administracion",
+      items: [
+        {
+          text: "Gestión de clientes"
+        },
+        {
+          text: "Areas",
+          active: true
+        }
+      ],
+     dropzoneOptions: {
+        thumbnailWidth: 150,
+        maxFilesize: 0.5,
+        headers: { "My-Awesome-Header": "header value" }
+      },
+      ver:false,
+      url:"",
+      url_firma:"",
+      modal: true,
+      file:null,
+      firma:null,
+      email: "",
+      password: "",
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 10,
+      pageOptions: [10, 25, 50, 100],
+      filter: null,
+      filterOn: [],
+      sortBy: "age",
+      sortDesc: false,
+      fields: ["nombre","descripcion","actions"],
+      areas: [], 
+      editMode:false,
+      form:{
+        'id': '',
+        'nombre':'',
+        'descripcion':'',
+        'status':'',
+        'documento':'',
+        'created_at':'',
+        'updated_at':'',
+        'cliente_id':'',
+      }
+    }
+  },
+
+  created(){
+    this.listarUsers();
+  },
+  methods: {
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length;
+      this.currentPage = 1;
+    }, 
+    ...mapActions(['guardarUsuario']),
+    switchLoc(){
+      if (!this.editMode) {
+        this.$refs.form.validate().then(esValido => {
+            if (esValido) {
+              this.agregarArea();
+            } else {}
+          });        
+        }else{
+          this.$refs.form.validate().then(esValido => {
+          if (esValido) {
+            this.editarAreas();
+          } else {
+        }});
+      }
+    },
+   async agregarPlantilla(){
+     let data = new FormData();
+      var formulario = this.form;
+        for (var key in formulario) {
+          data.append(key,formulario[key]);
+        }
+       await this.axios.post('api/plantillas', data, {
+           headers: {
+            'Content-Type': 'multipart/form-data'
+           }}).then(response => {
+            if (response.status==200) {
+               this.$swal(
+                   'Agregado exito!',
+                    '',
+                    'success');
+               this.listarAreas();
+               this.$root.$emit("bv::hide::modal", "modal", "#btnShow");
+               ///limpiar el formulario
+              this.resete();
+              }
+            }).catch(e => {
+              console.log(e.response.data.menssage);
+              this.$swal(e.response.data);
+          });
+      },
+    async editarPlantillas(){
+     let data = new FormData();
+       var formulario = this.form;
+        for (var key in formulario) {
+          data.append(key,formulario[key]);
+        }
+        await this.axios.put('api/plantillas', data).then(response => {
+            if (response.status==200) {
+               this.$swal('Editado con exito','','success');
+               this.listarAreas();
+               this.$root.$emit("bv::hide::modal", "modal", "#btnShow");
+               ///limpiar el formulario
+              this.resete();
+              }
+            }).catch(e => {
+                this.$swal('ocurrio un problema','','warning');
+            });
+     },
+     async eliminarPlantillas(id){
+        let data = new FormData();
+        data.append('id',id);
+        await this.axios.post('api/plantillas/delete',data, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }}).then(response => {
+              if (response.status==200) {
+                this.$swal(
+                    'Eliminado con exito!',
+                      '',
+                      'success'
+                );
+                this.listarAreas();
+                }
+              }).catch(e => {
+                console.log(e.response.data.menssage);
+                this.$swal(e.response.data);
+          });
+      }, 
+      eliminarPlantilla(id){
+        this.$swal({
+          title: 'Desea borrar esta area?',
+          icon: 'question',
+          iconHtml: '',
+          confirmButtonText: 'Si',
+          cancelButtonText: 'No',
+          showCancelButton: true,
+          showCloseButton: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.eliminarAreas(id);
+          }
+        })
+      },
+      resete(){
+        var formulario = this.form;
+        for (var key in formulario) {
+             this.form[key]="";
+       }
+       this.form.cliente_id=this.cliente.id;
+      },
+      setear(id) {
+        for (let index = 0; index < this.areas.length; index++) {
+          if (this.areas[index].id===id) {
+            this.form.id=this.areas[index].id;
+            this.form.nombre=this.areas[index].nombre;
+            this.form.descripcion=this.areas[index].descripcion;
+            this.$root.$emit("bv::show::modal", "modal", "#btnShow");
+            return;
+          }
+        }
+      },
+    async listarPlantillas(){
+     let data = new FormData();
+     data.append('cliente_id',this.cliente.id);
+       await this.axios.post('api/plantillas/listar',data)
+        .then((response) => {
+          this.areas = response.data;
+        })
+        .catch((e)=>{
+          console.log('error' + e);
+        })
+      },
+      setEmail(){
+        this.form.username=this.form.email;
+        console.log("holas");
+      },
+      setRoles(){
+        if (this.form.tipo==="Administrador") {
+            this.form.codigo="";
+            this.form.entidad="No";
+            this.form.cargo=""
+            this.form.roles="3"
+        } else if(this.form.tipo==="Coordinador") {
+            this.form.entidad="No";
+            this.form.cargo="";
+            this.form.roles="2"
+        }else{
+            this.form.roles="1"
+        }
+      },
+      onFileChange(e) {
+        const file = e.target.files[0];
+        this.url = URL.createObjectURL(file);
+      },
+      toggleModal () {
+        this.modal = !this.modal
+      },
+      session(){
+        if (localStorage.getItem('token')) {
+          const token=localStorage.getItem('token');
+          this.guardarUsuario(token);
+          }else{
+          this.$router.push({ name: 'Home' });
+          }
+        }
+  },
+    created(){
+        this.session();
+        this.listarPlantillas();
+      },
+   watch: {
+      cliente: function () {
+
+        this.title=this.cliente.nombre_prestador;
+      },
+    },
+    computed: {
+      ...mapState(['usuarioDB','cliente']),
+    rows() {
+      return this.areas.length;
+    },
+  },
+}
+</script>
