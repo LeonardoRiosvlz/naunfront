96416b3e9aa3b68ebe33edde930240be81ff29a0
Luis Angel <luis.angel151199@gmail.com>
2021-06-20 17:45:29 -0400

    estandares and grupos de estandares created

--- src/router/routes.js
+++ src/router/routes.js
@@ -414,6 +414,40 @@ export default [
         },
     },
     {
+        path: '/gruposestandares',
+        name: 'gruposestandares',
+        component: () => import('../views/pages/admin/gruposestandares'),
+        meta: {
+            beforeEnter: (to, from, next) => {
+                if (!store.state.usuarioDB) {
+                    console.log(store.state.usuarioDB);
+                    // Redirect to the home page instead
+                    next({ name: 'login' })
+                } else {
+                    // Continue to the login page
+                   next()
+                }
+            }
+        },
+    },
+    {
+        path: '/estandares',
+        name: 'estandares',
+        component: () => import('../views/pages/admin/estandares'),
+        meta: {
+            beforeEnter: (to, from, next) => {
+                if (!store.state.usuarioDB) {
+                    console.log(store.state.usuarioDB);
+                    // Redirect to the home page instead
+                    next({ name: 'login' })
+                } else {
+                    // Continue to the login page
+                   next()
+                }
+            }
+        },
+    },
+    {
         path: '/notificaciones',
         name: 'notificaciones',
         component: () => import('../views/pages/admin/notificaciones'),
