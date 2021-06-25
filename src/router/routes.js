b1a3e4267c06a9f018972e5c198dd50f8f1ff3ef
Luis Angel <luis.angel151199@gmail.com>
2021-06-25 14:56:57 -0400

    detalles subprocesos created and fixed

--- src/router/routes.js
+++ src/router/routes.js
@@ -483,6 +483,23 @@ export default [
         },
     },
     {
+        path: '/detalles-subprocesos/:id',
+        name: 'detallesubprocesos',
+        component: () => import('../views/pages/admin/detallesubprocesos'),
+        meta: {
+            beforeEnter: (to, from, next) => {
+                if (!store.state.usuarioDB) {
+                    console.log(store.state.usuarioDB);
+                    // Redirect to the home page instead
+                    next({ name: 'login' })
+                } else {
+                    // Continue to the login page
+                   next()
+                }
+            }
+        },
+    },
+    {
         path: '/notificaciones',
         name: 'notificaciones',
         component: () => import('../views/pages/admin/notificaciones'),
